bb51289d9ebfd60317828e59874afa190106670f
davitebralidze <dav.ebralidze@gmail.com>
2023-11-25 21:54:20 +0400

    POMStructure

--- src/main/java/pageElements/FirstPageElements.java
+++ src/main/java/pageElements/FirstPageElements.java
@@ -8,9 +8,14 @@ import org.openqa.selenium.WebElement;
 @Getter
 public class FirstPageElements {
 
+    WebDriver driver;
 
-    private final By searchBarPath = By.xpath("//*[@id=\"APjFqb\"]");
-    private final By searchButtonPath = By.xpath("/html/body/div[1]/div[3]/form/div[1]/div[1]/div[4]/center/input[1]");
+    public FirstPageElements(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    private final WebElement searchBar = driver.findElement(By.xpath("//*[@id=\"APjFqb\"]"));
+    private final WebElement searchButton = driver.findElement(By.xpath("/html/body/div[1]/div[3]/form/div[1]/div[1]/div[4]/center/input[1]"));
 
 
 }
--- src/main/java/pageSteps/FirstPageSteps.java
+++ src/main/java/pageSteps/FirstPageSteps.java
@@ -7,14 +7,13 @@ import pageElements.FirstPageElements;
 
 public class FirstPageSteps extends FirstPageElements {
 
-    private WebDriver driver;
 
     public FirstPageSteps(WebDriver driver) {
-        this.driver = driver;
+        super(driver);
     }
 
     @Step("Give data to a search bar")
     public void inputSearchData(String input) {
-        driver.findElement(getSearchBarPath()).sendKeys(input);
+        getSearchBar().sendKeys(input);
     }
 }
