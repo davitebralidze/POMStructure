a938196c79da4944ec701865fadddf4ccc9ab8a3
davitebralidze <dav.ebralidze@gmail.com>
2024-01-05 16:00:20 +0400

    POMStructure

--- src/test/java/APITestDELETE.java
+++ src/test/java/APITestDELETE.java
@@ -3,7 +3,6 @@ import io.restassured.response.Response;
 public class APITestDELETE {
     public static void main(String[] args) {
 
-
         String ENDPOINT = "/api/users/2";
         RestAssured.baseURI = "https://reqres.in";
         Response response = RestAssured.delete(ENDPOINT);
