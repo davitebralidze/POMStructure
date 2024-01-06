fff198208179ba6a371793dee07674ad19f9df9c
davitebralidze <dav.ebralidze@gmail.com>
2024-01-06 18:15:02 +0400

    POMStructure

--- src/test/java/APITestPOST.java
+++ src/test/java/APITestPOST.java
@@ -13,7 +13,6 @@ public class APITestPOST {
                         "    \"RememberMe\" : false\n" +
                         "}";
 
-
         Response response = RestAssured.given().contentType(ContentType.JSON).body(requestBody).post("https://api.techwave.ge/api/Account/SignIn");
 
         System.out.println(response.getStatusCode());
