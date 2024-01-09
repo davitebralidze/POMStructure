518193d892f01a8eaf8283426815884dcc2eb891
davitebralidze <dav.ebralidze@gmail.com>
2024-01-09 17:46:10 +0400

    POMStructure

---
+++ src/test/java/FirstPageTestWithoutExtend.java
@@ -0,0 +1,10 @@
+import org.testng.annotations.Test;
+
+public class FirstPageTestWithoutExtend {
+
+    @Test
+    public void test() {
+        System.out.println("FirstPageWithoutExtend");
+    }
+
+}
