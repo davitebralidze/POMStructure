c6b1896a07fbe1b1311561bcabf6b0440e6440f5
davitebralidze <dav.ebralidze@gmail.com>
2024-02-09 23:16:29 +0400

    POM Structure

--- src/test/java/ScratchCodes.java
+++ src/test/java/ScratchCodes.java
@@ -2,6 +2,5 @@ import java.time.LocalTime;
 public class ScratchCodes {
     public static void main(String[] args) {
         System.out.println("Write some test codes here");
-        System.out.println(LocalTime.now());
     }
 }
