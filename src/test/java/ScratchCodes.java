94398920e14f397f925605ce999fb33cc9c2154c
davitebralidze <dav.ebralidze@gmail.com>
2024-03-04 22:50:52 +0400

    Latest Modifies

--- src/test/java/ScratchCodes.java
+++ src/test/java/ScratchCodes.java
@@ -1,12 +1,8 @@
 import Util.Utils;
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-
-import java.time.LocalTime;
 
 public class ScratchCodes {
     public static void main(String[] args) throws InterruptedException {
