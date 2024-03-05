04dd79656c65c65eb9938a1fb168777b72c7ddfb
davitebralidze <dav.ebralidze@gmail.com>
2024-03-05 10:08:02 +0400

    Small changes

--- src/test/java/FirstPageWithoutBeforeTest.java
+++ src/test/java/FirstPageWithoutBeforeTest.java
@@ -11,7 +11,7 @@ public class FirstPageWithoutBeforeTest {
 
     @Test
     public void test() {
-        System.out.println("FirstPageWithoutExtend! ? # /");
+        System.out.println("FirstPageWithoutExtend!");
     }
 
 }
