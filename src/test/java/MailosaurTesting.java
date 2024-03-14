9fbb5ae0011e222ca7ad0075d757396f144ae47f
davitebralidze <dav.ebralidze@gmail.com>
2024-03-14 16:08:50 +0400

    Finishing work on mailosaur

--- src/test/java/MailosaurTesting.java
+++ src/test/java/MailosaurTesting.java
@@ -11,6 +11,7 @@ import org.testng.annotations.Test;
 
 import java.io.IOException;
 import java.time.Duration;
+import java.util.Calendar;
 import java.util.Date;
 
 public class MailosaurTesting {
@@ -18,11 +19,15 @@ public class MailosaurTesting {
     @Test
     public void testingMailosaur() throws MailosaurException, IOException {
 
-        Date myDate = new Date(1, 5, 4);
         MailosaurClient mailosaur = new MailosaurClient(PropertyLoader.returnConfigValue("MailosaurAPI"));
 
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.MINUTE, -525960);
+        Date pastDate = calendar.getTime();
+        //This is done to fetch all the emails for past 1 year (In minutes), as Date type does not work well
+
         MessageSearchParams params = new MessageSearchParams();
-        params.withServer(PropertyLoader.returnConfigValue("MailosaurServerId")).withReceivedAfter(myDate);
+        params.withServer(PropertyLoader.returnConfigValue("MailosaurServerId")).withReceivedAfter(pastDate);
 
         SearchCriteria criteria = new SearchCriteria();
         criteria.withSentTo("history-poetry@" + PropertyLoader.returnConfigValue("MailosaurServerDomain"));
