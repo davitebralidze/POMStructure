8fc76d5ae6b8c45fd971cd9c4d5e66b2a3b1bcca
davitebralidze <dav.ebralidze@gmail.com>
2024-03-29 17:19:43 +0400

    Changes on the config.properties function

--- src/main/java/Util/PropertyLoader.java
+++ src/main/java/Util/PropertyLoader.java
@@ -5,11 +5,13 @@ import java.io.InputStream;
 import java.util.Properties;
 
 public class PropertyLoader {
+    static String direction = "config.properties";
+
     public static String returnConfigValue(final String property) {
         Properties properties = new Properties();
 
         try (InputStream propFileInpStream = PropertyLoader.class.getClassLoader()
-                .getResourceAsStream("config.properties")) {
+                .getResourceAsStream(direction)) {
 
             properties.load(propFileInpStream);
             return properties.getProperty(property);
