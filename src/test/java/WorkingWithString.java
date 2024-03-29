e42b6b87b7df5f731252f737283e70df097da3ac
davitebralidze <dav.ebralidze@gmail.com>
2024-03-29 16:14:34 +0400

    Adding String regex to classes list

--- src/test/java/WorkingWithString.java
+++ src/test/java/WorkingWithString.java
@@ -5,5 +5,7 @@ public class WorkingWithString {
     public static void main(String[] args) {
         String stringToWorkOn = "Hello, I'm robot and my email is robot@email.com and you can write to me here";
         System.out.println(stringToWorkOn.split("is")[1].split("and")[0].trim());
+        //split() functions splits the string in array of strings (removes the string by which we split the text)
+        //trim() removes extra white spaces at the beginning and end
     }
 }
