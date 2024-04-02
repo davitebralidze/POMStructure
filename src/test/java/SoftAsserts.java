01cafac495096c12119b80f95823617464e76b64
davitebralidze <dav.ebralidze@gmail.com>
2024-04-02 11:09:27 +0400

    Adding Rest Assured methods instead or HttpURLConnection

--- src/test/java/SoftAsserts.java
+++ src/test/java/SoftAsserts.java
@@ -1,3 +1,5 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -13,22 +15,34 @@ public class SoftAsserts {
     public static void main(String[] args) throws IOException {
         SoftAssert softAssert = new SoftAssert();
 
+//        WebDriver driver = new ChromeDriver();
+//        String baseURL = "https://rahulshettyacademy.com/AutomationPractice";
+
+//        driver.get(baseURL);
+//        List<WebElement> links = driver.findElements(By.cssSelector("li.gf-li a"));
+//
+//        for (WebElement link : links) {
+//            String url = link.getAttribute("href");
+//            HttpURLConnection conn = (HttpURLConnection) new URL(url).openConnection();
+//            conn.setRequestMethod("HEAD");
+//            conn.connect();
+//            int responseCode = conn.getResponseCode();
+//            softAssert.assertTrue(responseCode < 400 ,
+//                    "The status code was greater than 400 for the link: " +
+//                            link.getAttribute("href") + " and the status code was "+responseCode);
+//        }
+//
+//        softAssert.assertAll();
+
         WebDriver driver = new ChromeDriver();
-
-        driver.get("https://rahulshettyacademy.com/AutomationPractice");
+        String baseURL = "https://rahulshettyacademy.com/AutomationPractice";
+        driver.get(baseURL);
         List<WebElement> links = driver.findElements(By.cssSelector("li.gf-li a"));
-
         for (WebElement link : links) {
             String url = link.getAttribute("href");
-            HttpURLConnection conn = (HttpURLConnection) new URL(url).openConnection();
-            conn.setRequestMethod("HEAD");
-            conn.connect();
-            int responseCode = conn.getResponseCode();
-            softAssert.assertTrue(responseCode < 400 ,
-                    "The status code was greater than 400 for the link: " +
-                            link.getAttribute("href") + " and the status code was "+responseCode);
+            Response response = RestAssured.get(url);
+            System.out.println(response.getStatusCode());
         }
 
-        softAssert.assertAll();
     }
 }
