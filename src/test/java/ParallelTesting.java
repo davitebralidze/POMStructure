44194c4bfd303a4e44fbf9e0987121fe3c8d27fb
davitebralidze <dav.ebralidze@gmail.com>
2024-04-10 10:29:35 +0400

    Adding Parallel Tests testng file to the proejct

---
+++ src/test/java/ParallelTesting.java
@@ -0,0 +1,33 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class ParallelTesting {
+
+    WebDriver driver;
+
+    @Parameters("browser")
+    @Test
+    public void parallelTest(String browser) throws InterruptedException{
+
+        System.out.println("Browser name i " + browser);
+        System.out.println("Thread id: " + Thread.currentThread().getId());
+
+        if (browser.equalsIgnoreCase("chrome")) {
+            driver = new ChromeDriver();
+            Thread.sleep(5000);
+        } else if (browser.equalsIgnoreCase("firefox")) {
+            driver = new FirefoxDriver();
+            Thread.sleep(5000);
+        } else {
+            System.out.println("Browser not found");
+        }
+
+        driver.quit();
+
+    }
+
+
+}
