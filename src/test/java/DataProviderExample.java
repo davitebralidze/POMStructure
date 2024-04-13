a0cedeb03d54f69d8e555b763d2b1cad80890421
davitebralidze <dav.ebralidze@gmail.com>
2024-04-13 18:19:52 +0400

    Adding DataProvider comment for clarification

--- src/test/java/DataProviderExample.java
+++ src/test/java/DataProviderExample.java
@@ -2,13 +2,15 @@ import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class DataProviderExample {
+
+    //REMEMBER! DataProvider should always be in the same class where the tests are locetad!
+
     @DataProvider(name = "loginData")
     public Object[][] provideData() {
         return new Object[][]{
                 {"user1", "password1"},
                 {"user2", "password2"},
-                {"user3", "password3"},
-        };
+                {"user3", "password3"}};
     }
 
     @Test(dataProvider = "loginData")
