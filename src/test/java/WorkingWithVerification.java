2d0ce328c9dba8086b793a6ddc4fdf7a6803e509
davitebralidze <dav.ebralidze@gmail.com>
2024-04-15 10:40:50 +0400

    Verification code, making code more suitable for various cases

--- src/test/java/WorkingWithVerification.java
+++ src/test/java/WorkingWithVerification.java
@@ -9,17 +9,17 @@ public class WorkingWithVerification {
 
     public static void main(String[] args) throws InterruptedException{
 
+        String receivedCode = "435654";
 
         WebDriver driver = new ChromeDriver();
 
         driver.get("https://qaplayground.dev/apps/verify-account/");
         Thread.sleep(5000);
         List<WebElement> elements = driver.findElements(By.className("code"));
-        int[] code = {5, 4, 2, 7, 1, 2};
         int verificationBoxes = elements.size();
 
         for (int i = 0; i < verificationBoxes; i++) {
-            elements.get(i).sendKeys(String.valueOf(code[i]));
+            elements.get(i).sendKeys(String.valueOf(receivedCode.toCharArray()[i]));
         }
 
 
