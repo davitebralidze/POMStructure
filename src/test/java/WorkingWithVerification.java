decb7a1c2567629df1ed39b4d876d82cd8ba500d
davitebralidze <dav.ebralidze@gmail.com>
2024-04-15 10:29:49 +0400

    Verification code injection

---
+++ src/test/java/WorkingWithVerification.java
@@ -0,0 +1,28 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
+public class WorkingWithVerification {
+
+    public static void main(String[] args) throws InterruptedException{
+
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://qaplayground.dev/apps/verify-account/");
+        Thread.sleep(5000);
+        List<WebElement> elements = driver.findElements(By.className("code"));
+        int[] code = {5, 4, 2, 7, 1, 2};
+
+        for (int i = 0; i < 6; i++) {
+            elements.get(i).sendKeys(String.valueOf(code[i]));
+        }
+
+
+    }
+
+
+}
