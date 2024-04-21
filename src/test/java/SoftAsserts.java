dcaae6889d5d622bf37b51327e4d45487044a92b
davitebralidze <dav.ebralidze@gmail.com>
2024-04-21 23:16:48 +0400

    Learning new custom error message for allure

--- src/test/java/SoftAsserts.java
+++ src/test/java/SoftAsserts.java
@@ -15,29 +15,26 @@ public class SoftAsserts {
     public static void main(String[] args) throws IOException {
         SoftAssert softAssert = new SoftAssert();
 
-//        WebDriver driver = new ChromeDriver();
-//        String baseURL = "https://rahulshettyacademy.com/AutomationPractice";
-
-//        driver.get(baseURL);
-//        List<WebElement> links = driver.findElements(By.cssSelector("li.gf-li a"));
-//
-//        for (WebElement link : links) {
-//            String url = link.getAttribute("href");
-//            HttpURLConnection conn = (HttpURLConnection) new URL(url).openConnection();
-//            conn.setRequestMethod("HEAD");
-//            conn.connect();
-//            int responseCode = conn.getResponseCode();
-//            softAssert.assertTrue(responseCode < 400 ,
-//                    "The status code was greater than 400 for the link: " +
-//                            link.getAttribute("href") + " and the status code was "+responseCode);
-//        }
-//
-//        softAssert.assertAll();
-
         WebDriver driver = new ChromeDriver();
         String baseURL = "https://rahulshettyacademy.com/AutomationPractice";
+
         driver.get(baseURL);
         List<WebElement> links = driver.findElements(By.cssSelector("li.gf-li a"));
+
+        for (WebElement link : links) {
+            String url = link.getAttribute("href");
+            HttpURLConnection conn = (HttpURLConnection) new URL(url).openConnection();
+            conn.setRequestMethod("HEAD");
+            conn.connect();
+            int responseCode = conn.getResponseCode();
+            softAssert.assertTrue(responseCode < 400 ,
+                    "The status code was greater than 400 for the link: " +
+                            link.getAttribute("href") + " and the status code was "+responseCode);
+        }
+
+        softAssert.assertAll();
+
+        driver.get(baseURL);
         for (WebElement link : links) {
             String url = link.getAttribute("href");
             Response response = RestAssured.get(url);
