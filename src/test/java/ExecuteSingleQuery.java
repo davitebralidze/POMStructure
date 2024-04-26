64ba1be4923d017d2de6c6baf9d7f9a498574a7a
davitebralidze <dav.ebralidze@gmail.com>
2024-04-27 00:46:31 +0400

    Creating quary executor

--- src/test/java/ExecuteSingleQuery.java
+++ src/test/java/ExecuteSingleQuery.java
@@ -4,8 +4,8 @@ import java.sql.SQLException;
 
 public class ExecuteSingleQuery {
     public static void main(String[] args) throws SQLException {
-
         System.out.println(QueryExecutioner.selectSpecificDataFromColumn("SELECT * FROM music.albumview WHERE album_name='18 Singles'", "song_title"));
-
+        QueryExecutioner.executeDataAlteringQuery("UPDATE `music`.`songs` SET `song_title` = 'I will follow' WHERE `song_id` = 598;");
+        System.out.println(QueryExecutioner.selectSpecificDataFromColumn("SELECT * FROM music.albumview WHERE album_name='18 Singles'", "song_title"));
     }
 }
