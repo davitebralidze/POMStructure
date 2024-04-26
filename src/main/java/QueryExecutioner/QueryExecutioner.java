5cc150ceebcda954c0454fa237a827ab5da48b0a
davitebralidze <dav.ebralidze@gmail.com>
2024-04-26 22:43:52 +0400

    Creating quary executor

---
+++ src/main/java/QueryExecutioner/QueryExecutioner.java
@@ -0,0 +1,55 @@
+package QueryExecutioner;
+
+import com.mysql.cj.jdbc.MysqlDataSource;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
+
+public class QueryExecutioner {
+    public static String selectSpecificDataFromColumn(String query, String columnName) {
+        Properties properties = new Properties();
+        try {
+            properties.load(Files.newInputStream(Path.of("DBCredentials.properties"), StandardOpenOption.READ));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        var dataSource = new MysqlDataSource();
+        dataSource.setServerName(properties.getProperty("serverName"));
+        dataSource.setPort(Integer.parseInt(properties.getProperty("port")));
+        dataSource.setDatabaseName(properties.getProperty("databaseName"));
+
+        try (var connection = dataSource.getConnection(properties.getProperty("userName"), properties.getProperty("password")); Statement statement = connection.createStatement()) {
+            ResultSet resultSet = statement.executeQuery(query);
+            resultSet.next();
+            return resultSet.getString(columnName);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void executeDataAlteringQuery(String query) {
+        Properties properties = new Properties();
+        try {
+            properties.load(Files.newInputStream(Path.of("DBCredentials.properties"), StandardOpenOption.READ));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        var dataSource = new MysqlDataSource();
+        dataSource.setServerName(properties.getProperty("serverName"));
+        dataSource.setPort(Integer.parseInt(properties.getProperty("port")));
+        dataSource.setDatabaseName(properties.getProperty("databaseName"));
+
+        try (var connection = dataSource.getConnection(properties.getProperty("userName"), properties.getProperty("password")); Statement statement = connection.createStatement()) {
+            statement.executeUpdate(query);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
---
+++ src/test/java/ExecuteSingleQuery.java
@@ -0,0 +1,11 @@
+import QueryExecutioner.QueryExecutioner;
+
+import java.sql.SQLException;
+
+public class ExecuteSingleQuery {
+    public static void main(String[] args) throws SQLException {
+
+        System.out.println(QueryExecutioner.selectSpecificDataFromColumn("SELECT * FROM music.albumview WHERE album_name='18 Singles'", "song_title"));
+
+    }
+}
