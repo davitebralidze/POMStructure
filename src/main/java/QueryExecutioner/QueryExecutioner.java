404c621a03f6a251b8498fb544ed5075e8b55f2c
davitebralidze <dav.ebralidze@gmail.com>
2024-04-29 11:13:27 +0400

    Making code universal for databases

--- src/main/java/QueryExecutioner/QueryExecutioner.java
+++ src/main/java/QueryExecutioner/QueryExecutioner.java
@@ -1,5 +1,6 @@
 package QueryExecutioner;
 
+import com.microsoft.sqlserver.jdbc.SQLServerDataSource;
 import com.mysql.cj.jdbc.MysqlDataSource;
 
 import java.io.IOException;
@@ -12,44 +13,51 @@ import java.sql.Statement;
 import java.util.Properties;
 
 public class QueryExecutioner {
-    public static String selectSpecificDataFromColumn(String query, String columnName) {
-        Properties properties = new Properties();
-        try {
-            properties.load(Files.newInputStream(Path.of("DBCredentials.properties"), StandardOpenOption.READ));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        var dataSource = new MysqlDataSource();
-        dataSource.setServerName(properties.getProperty("serverName"));
-        dataSource.setPort(Integer.parseInt(properties.getProperty("port")));
-        dataSource.setDatabaseName(properties.getProperty("databaseName"));
+    public static class MySQL {
+        public static String selectSpecificDataFromColumn(String query, String columnName) {
+            Properties properties = new Properties();
+            try {
+                properties.load(Files.newInputStream(Path.of("DBCredentials.properties"), StandardOpenOption.READ));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            var dataSource = new MysqlDataSource();
+            dataSource.setServerName(properties.getProperty("serverName"));
+            dataSource.setPort(Integer.parseInt(properties.getProperty("port")));
+            dataSource.setDatabaseName(properties.getProperty("databaseName"));
 
-        try (var connection = dataSource.getConnection(properties.getProperty("userName"), properties.getProperty("password")); Statement statement = connection.createStatement()) {
-            ResultSet resultSet = statement.executeQuery(query);
-            resultSet.next();
-            return resultSet.getString(columnName);
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
+            try (var connection = dataSource.getConnection(properties.getProperty("userName"), properties.getProperty("password")); Statement statement = connection.createStatement()) {
+                ResultSet resultSet = statement.executeQuery(query);
+                resultSet.next();
+                return resultSet.getString(columnName);
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        public static void executeDataAlteringQuery(String query) {
+            Properties properties = new Properties();
+            try {
+                properties.load(Files.newInputStream(Path.of("DBCredentials.properties"), StandardOpenOption.READ));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            var dataSource = new MysqlDataSource();
+            dataSource.setServerName(properties.getProperty("serverName"));
+            dataSource.setPort(Integer.parseInt(properties.getProperty("port")));
+            dataSource.setDatabaseName(properties.getProperty("databaseName"));
+
+            try (var connection = dataSource.getConnection(properties.getProperty("userName"), properties.getProperty("password")); Statement statement = connection.createStatement()) {
+                statement.executeUpdate(query);
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
         }
     }
+    //TODO Add MS and Postgre connections
+    public static class MSSQL {}
+    public static class PostgreSQL{}
 
-    public static void executeDataAlteringQuery(String query) {
-        Properties properties = new Properties();
-        try {
-            properties.load(Files.newInputStream(Path.of("DBCredentials.properties"), StandardOpenOption.READ));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        var dataSource = new MysqlDataSource();
-        dataSource.setServerName(properties.getProperty("serverName"));
-        dataSource.setPort(Integer.parseInt(properties.getProperty("port")));
-        dataSource.setDatabaseName(properties.getProperty("databaseName"));
-
-        try (var connection = dataSource.getConnection(properties.getProperty("userName"), properties.getProperty("password")); Statement statement = connection.createStatement()) {
-            statement.executeUpdate(query);
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
     }
 
-}
+
