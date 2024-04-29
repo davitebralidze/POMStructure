8777ea44425281d02df13fb8ec3dbd484a4213ba
davitebralidze <dav.ebralidze@gmail.com>
2024-04-29 15:19:32 +0400

    Making code universal for databases

--- src/main/java/QueryExecutioner/QueryExecutioner.java
+++ src/main/java/QueryExecutioner/QueryExecutioner.java
@@ -3,6 +3,7 @@ package QueryExecutioner;
 import com.microsoft.sqlserver.jdbc.SQLServerDataSource;
 import com.mysql.cj.jdbc.MysqlDataSource;
 
+import javax.sql.DataSource;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -15,27 +16,19 @@ import java.util.Properties;
 public class QueryExecutioner {
     public static class MySQL {
         public static String selectSpecificDataFromColumn(String query, String columnName) {
+
             Properties properties = new Properties();
             try {
                 properties.load(Files.newInputStream(Path.of("DBCredentials.properties"), StandardOpenOption.READ));
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
+
             var dataSource = new MysqlDataSource();
             dataSource.setServerName(properties.getProperty("serverName"));
             dataSource.setPort(Integer.parseInt(properties.getProperty("port")));
             dataSource.setDatabaseName(properties.getProperty("databaseName"));
-
-            try (var connection = dataSource.getConnection(properties.getProperty("userName"),
-                    properties.getProperty("password"));
-                 Statement statement = connection.createStatement()) {
-
-                ResultSet resultSet = statement.executeQuery(query);
-                resultSet.next();
-                return resultSet.getString(columnName);
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
-            }
+            return executeScriptForSelect(dataSource, query, columnName, properties);
 
         }
 
@@ -50,12 +43,8 @@ public class QueryExecutioner {
             dataSource.setServerName(properties.getProperty("serverName"));
             dataSource.setPort(Integer.parseInt(properties.getProperty("port")));
             dataSource.setDatabaseName(properties.getProperty("databaseName"));
+            executeScriptForAlter(dataSource, properties, query);
 
-            try (var connection = dataSource.getConnection(properties.getProperty("userName"), properties.getProperty("password")); Statement statement = connection.createStatement()) {
-                statement.executeUpdate(query);
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
-            }
         }
     }
     public static class MSSQL {
@@ -71,16 +60,7 @@ public class QueryExecutioner {
             dataSource.setPortNumber(Integer.parseInt(properties.getProperty("port")));
             dataSource.setDatabaseName(properties.getProperty("databaseName"));
 
-            try (var connection = dataSource.getConnection(properties.getProperty("userName"),
-                    properties.getProperty("password"));
-                 Statement statement = connection.createStatement()) {
-
-                ResultSet resultSet = statement.executeQuery(query);
-                resultSet.next();
-                return resultSet.getString(columnName);
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
-            }
+            return executeScriptForSelect(dataSource, query, columnName, properties);
 
         }
 
@@ -95,17 +75,33 @@ public class QueryExecutioner {
             dataSource.setServerName(properties.getProperty("serverName"));
             dataSource.setPortNumber(Integer.parseInt(properties.getProperty("port")));
             dataSource.setDatabaseName(properties.getProperty("databaseName"));
-
-            try (var connection = dataSource.getConnection(properties.getProperty("userName"), properties.getProperty("password")); Statement statement = connection.createStatement()) {
-                statement.executeUpdate(query);
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
-            }
+            executeScriptForAlter(dataSource, properties, query);
         }
     }
     //TODO Add Postgre connections
     public static class PostgreSQL{}
 
+
+    private static String executeScriptForSelect (DataSource dataSource, String query, String columnName, Properties properties) {
+
+        try (var connection = dataSource.getConnection(properties.getProperty("userName"),
+                properties.getProperty("password"));
+             Statement statement = connection.createStatement()) {
+
+            ResultSet resultSet = statement.executeQuery(query);
+            resultSet.next();
+            return resultSet.getString(columnName);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
+    private static void executeScriptForAlter (DataSource dataSource, Properties properties, String query) {
+        try (var connection = dataSource.getConnection(properties.getProperty("userName"), properties.getProperty("password")); Statement statement = connection.createStatement()) {
+            statement.executeUpdate(query);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
 
 
