e103078b73b5eee5551546aa657337ee7016cf45
davitebralidze <dav.ebralidze@gmail.com>
2024-04-29 15:26:29 +0400

    Making code universal for databases

--- src/test/java/ExecuteSingleQuery.java
+++ src/test/java/ExecuteSingleQuery.java
@@ -5,6 +5,6 @@ import java.sql.SQLException;
 public class ExecuteSingleQuery {
     public static void main(String[] args) throws SQLException {
         System.out.println(QueryExecutioner.MySQL.selectSpecificDataFromColumn("SELECT * FROM music.albumview WHERE album_name='18 Singles'", "song_title"));
-        System.out.println(QueryExecutioner.MSSQL.selectSpecificDataFromColumn("Select * From AspNetUsers Where Email like 'davit%'", "TenantId"));
+//        System.out.println(QueryExecutioner.MSSQL.selectSpecificDataFromColumn("Select * From AspNetUsers Where Email like 'davit%'", "TenantId"));
     }
 }
