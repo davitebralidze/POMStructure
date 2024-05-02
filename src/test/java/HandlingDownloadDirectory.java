75c49c3c32d90088fd747ceaa84cf748582cb275
davitebralidze <dav.ebralidze@gmail.com>
2024-05-02 15:35:33 +0400

    Download default location and download process

--- src/test/java/HandlingDownloadDirectory.java
+++ src/test/java/HandlingDownloadDirectory.java
@@ -1,12 +1,24 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class HandlingDownloadDirectory {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         ChromeOptions options = new ChromeOptions();
+
         Map<String , Object> prefs = new HashMap<String , Object>();
-        prefs.put("download.default_directory" , "Downloads");
+        prefs.put("download.default_directory" , "C:\\Users\\User\\IdeaProjects\\POMStructure\\Downloads");
+        options.setExperimentalOption("prefs", prefs);
+
+        WebDriver driver = new ChromeDriver(options);
+
+        driver.get("https://qaplayground.dev/apps/download/");
+        driver.findElement(By.xpath("//*[@id=\"file\"]")).click();
+        Thread.sleep(5000);
+
     }
 }
