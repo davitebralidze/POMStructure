2c1f55f1d34f348e55a69b399419fb0957ed36b6
David Ebralidze <dav.ebralidze@gmail.com>
2024-06-01 20:39:41 +0400

    tab handling

---
+++ src/test/java/BrowserWindowsHandle2.java
@@ -0,0 +1,33 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BrowserWindowsHandle2 {
+    public static void main(String[] args) {
+
+        WebDriver driver = new ChromeDriver();
+        Actions actions = new Actions(driver);
+
+        driver.get("https://demo.nopcommerce.com/");
+        driver.manage().window().maximize();
+
+        WebElement reg_element = driver.findElement(By.xpath("/html/body/div[6]/div[1]/div[1]/div[2]/div[1]/ul/li[1]/a"));
+
+        actions.keyDown(Keys.CONTROL).click(reg_element).keyUp(Keys.CONTROL).build().perform();
+
+        List<String> tabIDs = new ArrayList(driver.getWindowHandles());
+
+        driver.switchTo().window(tabIDs.get(1));
+
+        System.out.println(driver.findElement(By.xpath("//*[@id=\"main\"]/div/div/div/div[1]/h1")).getText());
+
+        driver.quit();
+
+    }
+}
