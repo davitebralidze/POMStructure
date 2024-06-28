1b4382077a7debdd8ee96f9badf967c6392c7ce4
David Ebralidze <dav.ebralidze@gmail.com>
2024-06-29 00:06:41 +0400

    Crating excel file in the project for counting test run number

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,13 +1,11 @@
 import Util.Utils;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.*;
 import org.testng.asserts.SoftAssert;
 
 import javax.xml.crypto.Data;
@@ -18,28 +16,27 @@ public class Test {
     public static void main(String[] args) throws InterruptedException {
 
 
+
         String URL = "https://flatrocktech.com/";
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless=new");
-        WebDriver driver = new ChromeDriver(options);
+        WebDriver driver = new ChromeDriver();
         Actions actions = new Actions(driver);
         SoftAssert softAssert = new SoftAssert();
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(50));
+        WebDriverWait explicitWait = new WebDriverWait(driver, Duration.ofSeconds(10));
 
-        driver.manage().window().maximize();
         driver.get(URL);
+        driver.manage().window().maximize();
 
-        Thread.sleep(5000);
 
-        //main box xPath
-
-        WebElement element = driver.findElement(By.xpath("//*[@id=\"__next\"]/div[1]/main/div[5]/div[2]/div/div[2]/div/div[1]/div[1]/div[2]/div"));
+        WebElement element = explicitWait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath
+                ("//*[@id=\"__next\"]/div[1]/main/div[5]/div[2]/div/div[2]/div/div[1]/div[1]/div[2]/div")));
 
         Utils.scrollToASpecificElement(driver, element);
-        actions.moveToElement(element).build().perform();
+        Utils.jsHover(driver, element);
 
-        List<WebElement> elements = driver.findElements(By.xpath("//*[@id=\"__next\"]/div[1]/main/div[5]/div[2]/div/div[2]/div/div[1]/div[1]/div[2]/div/div[1]/div[2]/div/ul//a"));
 
+
+
+        List<WebElement> elements = explicitWait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath("//*[@id=\"__next\"]/div[1]/main/div[5]/div[2]/div/div[2]/div/div[1]/div[1]/div[2]/div/div[1]/div[2]/div/ul//a")));
 
         for (WebElement link : elements) {
             String elementLink = link.getAttribute("href");
