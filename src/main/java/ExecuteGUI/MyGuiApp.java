afab3769c807d7fab3e65a81ed7f2296ca1a8770
Davit Ebralidze <Davit.Ebralidze@ventionteams.com>
2024-09-27 09:41:28 +0400

    Adding TODOs

--- src/main/java/ExecuteGUI/MyGuiApp.java
+++ src/main/java/ExecuteGUI/MyGuiApp.java
@@ -10,6 +10,10 @@ import java.io.*;
 
 public class MyGuiApp {
 
+    //TODO Add resolution dropdown
+    //TODO Add testng file selector dropdown (would be nice if dropdown will automatically fetch all the testng files)
+    //TODO If possible implement allure inside of the app window
+
     static String port;
 
     private static JTextArea consoleTextArea; // JTextArea for console output
