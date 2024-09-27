69cb812a18f55f2fa7e2f78208d522226fce26c4
Davit Ebralidze <Davit.Ebralidze@ventionteams.com>
2024-09-27 10:01:30 +0400

    Changes in the QueryExecutioner class

--- src/main/java/QueryExecutioner/QueryExecutioner.java
+++ src/main/java/QueryExecutioner/QueryExecutioner.java
@@ -2,7 +2,7 @@ package QueryExecutioner;
 
 import com.microsoft.sqlserver.jdbc.SQLServerDataSource;
 import com.mysql.cj.jdbc.MysqlDataSource;
-
+import org.postgresql.ds.PGSimpleDataSource;
 import javax.sql.DataSource;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -13,64 +13,68 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Properties;
 
+//TODO Add Postgre connection - Needs to be tested
+//TODO Test the database connections
+
+
 public class QueryExecutioner {
     public static class MySQL {
         public static String selectSpecificDataFromColumn(String query, String columnName) {
 
-            Properties properties = new Properties();
-            try {
-                properties.load(Files.newInputStream(Path.of("DBCredentials.properties"), StandardOpenOption.READ));
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-
+//            Properties properties = new Properties();
+//            try {
+//                properties.load(Files.newInputStream(Path.of("DBCredentials.properties"), StandardOpenOption.READ));
+//            } catch (IOException e) {
+//                throw new RuntimeException(e);
+//            }
+            Properties properties = loadProperties();
             var dataSource = new MysqlDataSource();
             dataSource.setServerName(properties.getProperty("serverName"));
             dataSource.setPort(Integer.parseInt(properties.getProperty("port")));
             dataSource.setDatabaseName(properties.getProperty("databaseName"));
             return executeScriptForSelect(dataSource, query, columnName, properties);
-
         }
 
         public static void executeDataAlteringQuery(String query) {
-            Properties properties = new Properties();
-            try {
-                properties.load(Files.newInputStream(Path.of("DBCredentials.properties"), StandardOpenOption.READ));
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
+//            Properties properties = new Properties();
+//            try {
+//                properties.load(Files.newInputStream(Path.of("DBCredentials.properties"), StandardOpenOption.READ));
+//            } catch (IOException e) {
+//                throw new RuntimeException(e);
+//            }
+            Properties properties = loadProperties();
             var dataSource = new MysqlDataSource();
             dataSource.setServerName(properties.getProperty("serverName"));
             dataSource.setPort(Integer.parseInt(properties.getProperty("port")));
             dataSource.setDatabaseName(properties.getProperty("databaseName"));
             executeScriptForAlter(dataSource, properties, query);
-
         }
     }
     public static class MSSQL {
         public static String selectSpecificDataFromColumn(String query, String columnName) {
-            Properties properties = new Properties();
-            try {
-                properties.load(Files.newInputStream(Path.of("DBCredentials.properties"), StandardOpenOption.READ));
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
+//            Properties properties = new Properties();
+//            try {
+//                properties.load(Files.newInputStream(Path.of("DBCredentials.properties"), StandardOpenOption.READ));
+//            } catch (IOException e) {
+//                throw new RuntimeException(e);
+//            }
+
+            Properties properties = loadProperties();
             var dataSource = new SQLServerDataSource();
             dataSource.setServerName(properties.getProperty("serverName"));
             dataSource.setPortNumber(Integer.parseInt(properties.getProperty("port")));
             dataSource.setDatabaseName(properties.getProperty("databaseName"));
-
             return executeScriptForSelect(dataSource, query, columnName, properties);
-
         }
 
         public static void executeDataAlteringQuery(String query) {
-            Properties properties = new Properties();
-            try {
-                properties.load(Files.newInputStream(Path.of("DBCredentials.properties"), StandardOpenOption.READ));
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
+//            Properties properties = new Properties();
+//            try {
+//                properties.load(Files.newInputStream(Path.of("DBCredentials.properties"), StandardOpenOption.READ));
+//            } catch (IOException e) {
+//                throw new RuntimeException(e);
+//            }
+            Properties properties = loadProperties();
             var dataSource = new SQLServerDataSource();
             dataSource.setServerName(properties.getProperty("serverName"));
             dataSource.setPortNumber(Integer.parseInt(properties.getProperty("port")));
@@ -78,15 +82,43 @@ public class QueryExecutioner {
             executeScriptForAlter(dataSource, properties, query);
         }
     }
-    //TODO Add Postgre connection
-    public static class PostgreSQL{}
+
+    public static class PostgreSQL{
+        public static String selectSpecificDataFromColumn(String query, String columnName) {
+//            Properties properties = new Properties();
+//            try {
+//                properties.load(Files.newInputStream(Path.of("DBCredentials.properties"), StandardOpenOption.READ));
+//            } catch (IOException e) {
+//                throw new RuntimeException(e);
+//            }
+            Properties properties = loadProperties();
+            var dataSource = new PGSimpleDataSource();
+            dataSource.setServerName(properties.getProperty("serverName"));
+            dataSource.setPortNumbers(new int[]{Integer.parseInt(properties.getProperty("port"))});
+            dataSource.setDatabaseName(properties.getProperty("databaseName"));
+            return executeScriptForSelect(dataSource, query, columnName, properties);
+        }
+
+        public static void executeDataAlteringQuery(String query) {
+//            Properties properties = new Properties();
+//            try {
+//                properties.load(Files.newInputStream(Path.of("DBCredentials.properties"), StandardOpenOption.READ));
+//            } catch (IOException e) {
+//                throw new RuntimeException(e);
+//            }
+            Properties properties = loadProperties();
+            var dataSource = new PGSimpleDataSource();
+            dataSource.setServerName(properties.getProperty("serverName"));
+            dataSource.setPortNumbers(new int[]{Integer.parseInt(properties.getProperty("port"))});
+            dataSource.setDatabaseName(properties.getProperty("databaseName"));
+            executeScriptForAlter(dataSource, properties, query);
+        }
+    }
 
     private static String executeScriptForSelect (DataSource dataSource, String query, String columnName, Properties properties) {
-
         try (var connection = dataSource.getConnection(properties.getProperty("userName"),
                 properties.getProperty("password"));
              Statement statement = connection.createStatement()) {
-
             ResultSet resultSet = statement.executeQuery(query);
             resultSet.next();
             return resultSet.getString(columnName);
@@ -94,6 +126,7 @@ public class QueryExecutioner {
             throw new RuntimeException(e);
         }
     }
+
     private static void executeScriptForAlter (DataSource dataSource, Properties properties, String query) {
         try (var connection = dataSource.getConnection(properties.getProperty("userName"), properties.getProperty("password")); Statement statement = connection.createStatement()) {
             statement.executeUpdate(query);
@@ -102,6 +135,14 @@ public class QueryExecutioner {
         }
     }
 
+    private static Properties loadProperties() {
+        Properties properties = new Properties();
+        try {
+            properties.load(Files.newInputStream(Path.of("DBCredentials.properties"), StandardOpenOption.READ));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } return properties;
+    }
 }
 
 
